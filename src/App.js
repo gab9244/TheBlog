2b3cd0c89dd48350f4076dd4e7810ea34a1e11eb
gab9244 <gab9244@gmail.com>
2024-04-24 14:47:48 -0300

    ff

--- src/App.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = require("react");
-var App = function () {
-    return (<div>App</div>);
-};
-exports.default = App;
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import LayOut from "./LayOut";
 import { Routes, Route } from "react-router-dom";
 import Section from "./Sections";
