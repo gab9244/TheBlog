ca139233b6cfcee8c2f0c2583bc1c17dcb5cf06e
Gabriel Francisco <gab9244@gmail.com>
2024-05-26 20:31:37 -0300

    trying to deploy

--- api/index.cjs
+++ api/index.cjs
@@ -24,7 +24,7 @@ const salt = bcrypt.genSaltSync(10)
 
 //Quando lidamos com credenciais/senhas e tokens é necessário colocar mais informações como definir o valor de credentials para true e fornecer a origem das solicitações http://localhost:5173
 //Se for local mude o valor de origin para 5173
-const allowedOrigins = ['https://theblog-cekp.onrender.com','https://theblog-1.onrender.com','http://localhost:4000'];
+const allowedOrigins = ['https://theblog-4agb.onrender.com/','https://theblog-api.onrender.com/','http://localhost:4000'];
 
 
 app.use(cors({
